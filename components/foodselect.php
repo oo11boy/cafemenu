5423433edffbb7fe52611564a19aaa1452a66e5c
oo11boy <editkhone@gmail.com>
2024-11-03 19:20:30 +0330

    feat:change font

--- components/foodselect.php
+++ components/foodselect.php
@@ -1,4 +1,4 @@
-<div class="w-full p-4 shadow-lg overflow-x-auto sticky top-0 bg-white z-10">
+<div class="w-full iransans p-4 shadow-lg overflow-x-auto sticky top-0 bg-white z-10">
     <div class="flex gap-4">
         <!-- آیتم برگر -->
         <div class="min-w-[100px] text-white whitespace-nowrap flex flex-col justify-center items-center">
@@ -57,7 +57,7 @@
 </div>
 
 
-<div class="viewfood flex flex-wrap gap-y-4  my-4 justify-between items-center">
+<div class="viewfood iransans flex flex-wrap gap-y-4  my-4 justify-between items-center">
     <div class="bg-[#e3e3e3]  flex flex-col w-[48%]  flex justify-center items-center p-2 rounded-xl">
         <img class="w-[80%]  h-[100px]"
             src="https://static.vecteezy.com/system/resources/thumbnails/036/303/390/small_2x/ai-generated-steaming-coffee-cup-hot-beverage-illustration-transparent-background-coffee-mug-clipart-hot-drink-graphic-brewed-coffee-icon-cafe-latte-png.png"
