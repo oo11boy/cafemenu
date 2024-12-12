483ed975b0011ee81bf8fb61b0b0dbe72a5fdce3
oo11boy <editkhone@gmail.com>
2024-12-12 19:14:17 +0330

    feat: fix specialoffers

--- components/specialOffers.php
+++ components/specialOffers.php
@@ -1,109 +1,138 @@
-<section class="special-offers">
-    <div class="chef-suggestion-title-container">
+<div class="spdiv">
 
-    <hr class="chef-suggestion-title-line">
+<div class="chef-suggestion-title-container">
+
+<hr class="chef-suggestion-title-line">
 <span class="chef-suggestion-title-text reyhaneh text-2xl absolute top-0">تخفیفات ویژه</span>
-    </div>
-    <div class="card-container-offer yekan">
-        <?php
-        $args = array(
-            'post_type' => 'food_item',
-            'meta_key' => 'discount_price', // فقط آیتم‌های دارای تخفیف
-            'meta_value' => '!=', // نمایش همه محصولاتی که قیمت تخفیف‌دار دارند
-            'meta_compare' => '!=', // چک می‌کند که مقدار تخفیف خالی نباشد
-            'posts_per_page' => -1, // نمایش همه محصولات تخفیف‌دار
-        );
+</div>
+<!-- Swiper -->
+<div class="swiper mySwiper">
 
-        $discount_offers_query = new WP_Query($args);
-   
-        if ($discount_offers_query->have_posts()) {
-            $has_posts = false; // متغیر کمکی برای بررسی اینکه آیا پست‌ها وجود دارند یا خیر
-            while ($discount_offers_query->have_posts()) {
-                $discount_offers_query->the_post();
-                $food_title = get_the_title();
-                $food_description = get_the_content();
-                $food_image = get_the_post_thumbnail_url();
-                $food_price = get_post_meta(get_the_ID(), 'food_price', true);
-                $discount_price = get_post_meta(get_the_ID(), 'discount_price', true); // قیمت با تخفیف
-                $discount_end_time = get_post_meta(get_the_ID(), 'discount_end_time', true); // زمان پایان تخفیف
-                $food_categories = wp_get_post_terms(get_the_ID(), 'food_category');
-                $category_ids = wp_list_pluck($food_categories, 'term_id');
-                
-                // چک می‌کنیم که قیمت تخفیف خالی نباشد و زمان تخفیف تمام نشده باشد
-                if (!empty($discount_price)) {
-                    // بررسی اینکه زمان تخفیف گذشته باشد یا نه
-                    $current_time = current_time('timestamp');
-                    $end_time = strtotime($discount_end_time);
-        
-                    if ($current_time > $end_time) {
-                        // اگر زمان تخفیف گذشته بود، محصول نمایش داده نمی‌شود
-                        continue;
-                    }
-                    ?>
-                    <div class="recipe-card-1 cursor-pointer" data-price="<?php echo esc_html($discount_price); ?>" id="card" data-categories="<?php echo implode(' ', $category_ids); ?>">
-                     <span class="line-through text-sm bg-[red] p-1 text-white absolute top-5 right-5"><?php echo esc_attr($food_price); ?> تومان</span> <!-- قیمت قبلی -->
-                     <p class="foodid hidden"><?php echo esc_attr(get_the_ID()); ?></p>
-                                     
-                    <div id="card__image">
-                            <img class="recipe-card-image object-cover" src="<?php echo esc_url($food_image ? $food_image : get_theme_image_url('dimg.png')); ?>" 
-                            alt="<?php echo esc_attr($food_title); ?>" />
-                        </div>
-        
-                        <div class="recipe-details-container" id="card__info">
-                            <div class="recipe-info" id="car__info--title">
-                                <h3><?php echo esc_html($food_title); ?></h3>
-                                <span class="hidden">
-                                    <?php echo esc_html($food_description) ?>
-                                </span>
-                                <div class="recipe-overview">
-                                    <p class="recipe-price flex flex-col">
-                                       <span class="text-white "><?php echo esc_attr($discount_price); ?> تومان</span> <!-- قیمت تخفیف‌دار -->
-                       
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-        
-                        <div class="bg-[gray] rounded-lg p-1 card__actions absolute top-0 left-5 flex justify-between items-center mt-2">
-                            <!-- دکمه + -->
-                            <button
-                                class="add-to-cart bg-[gray] text-white flex justify-center items-center text-2xl   h-[35px] w-[35px]  rounded-lg"
-                                data-food-id="<?php echo esc_attr(get_the_ID()); ?>"
-                                data-food-price="<?php echo esc_attr($discount_price); ?>"
-                                data-food-title="<?php echo esc_attr($food_title); ?>"
-                                data-food-image="<?php echo esc_attr($food_image); ?>">
-                                <i class="fa fa-plus text-lg" aria-hidden="true"></i>
-                            </button>
-        
-                            <!-- اینپوت تعداد و دکمه‌های + و - -->
-                            <div class="quantity-input flex justify-center !items-center hidden items-center">
-                                <button class="decrease bg-[gray] text-white flex justify-center items-center text-2xl h-[35px] w-[35px] rounded-lg">
-                                    <i class="fa fa-minus" aria-hidden="true"></i>
-                                </button>
-                                <input type="number" id="quantity_<?php echo esc_attr(get_the_ID()); ?>" name="quantity" min="1" value="1" class="h-[35px] w-[35px] countcart rounded-lg bg-[#F7F8F9] flex justify-center items-center text-center" readonly>
-                                <button class="increase bg-[gray] text-white flex justify-center items-center text-2xl h-[35px] w-[35px] rounded-lg">
-                                    <i class="fa fa-plus text-lg" aria-hidden="true"></i>
-                                </button>
+<div class="swiper-wrapper">
+<?php
+    $args = array(
+        'post_type' => 'food_item',
+        'meta_key' => 'discount_price', // فقط آیتم‌های دارای تخفیف
+        'meta_value' => '!=', // نمایش همه محصولاتی که قیمت تخفیف‌دار دارند
+        'meta_compare' => '!=', // چک می‌کند که مقدار تخفیف خالی نباشد
+        'posts_per_page' => -1, // نمایش همه محصولات تخفیف‌دار
+    );
+
+    $discount_offers_query = new WP_Query($args);
+
+    if ($discount_offers_query->have_posts()) {
+        $has_posts = false; // متغیر کمکی برای بررسی اینکه آیا پست‌ها وجود دارند یا خیر
+        while ($discount_offers_query->have_posts()) {
+            $discount_offers_query->the_post();
+            $food_title = get_the_title();
+            $food_description = get_the_content();
+            $food_image = get_the_post_thumbnail_url();
+            $food_price = get_post_meta(get_the_ID(), 'food_price', true);
+            $discount_price = get_post_meta(get_the_ID(), 'discount_price', true); // قیمت با تخفیف
+            $discount_end_time = get_post_meta(get_the_ID(), 'discount_end_time', true); // زمان پایان تخفیف
+            $food_categories = wp_get_post_terms(get_the_ID(), 'food_category');
+            $category_ids = wp_list_pluck($food_categories, 'term_id');
+            
+            // چک می‌کنیم که قیمت تخفیف خالی نباشد و زمان تخفیف تمام نشده باشد
+            if (!empty($discount_price)) {
+                // بررسی اینکه زمان تخفیف گذشته باشد یا نه
+                $current_time = current_time('timestamp');
+                $end_time = strtotime($discount_end_time);
+    
+                if ($current_time > $end_time) {
+                    // اگر زمان تخفیف گذشته بود، محصول نمایش داده نمی‌شود
+                    continue;
+                }
+                ?>
+
+  <div class="swiper-slide ">
+  <div class="w-full border border-gray-200 recipe-card-1 cursor-pointer" data-price="<?php echo esc_html($discount_price); ?>" id="card" data-categories="<?php echo implode(' ', $category_ids); ?>">
+                 <span class="line-through text-sm bg-[red] p-1 text-white absolute top-5 right-5"><?php echo esc_attr($food_price); ?> تومان</span> <!-- قیمت قبلی -->
+                 <p class="foodid hidden"><?php echo esc_attr(get_the_ID()); ?></p>
+                                 
+                <div id="card__image">
+                        <img class="recipe-card-image object-cover" src="<?php echo esc_url($food_image ? $food_image : get_theme_image_url('dimg.png')); ?>" 
+                        alt="<?php echo esc_attr($food_title); ?>" />
+                    </div>
+    
+                    <div class="recipe-details-container" id="card__info">
+                        <div class="recipe-info" id="car__info--title">
+                            <h3><?php echo esc_html($food_title); ?></h3>
+                            <span class="hidden">
+                                <?php echo esc_html($food_description) ?>
+                            </span>
+                            <div class="recipe-overview">
+                                <p class="recipe-price flex flex-col">
+                                   <span class="text-white "><?php echo esc_attr($discount_price); ?> تومان</span> <!-- قیمت تخفیف‌دار -->
+                   
+                                </p>
                             </div>
                         </div>
                     </div>
-                    <?php
-                     $has_posts = true; // اگر حلقه اجرا شد، متغیر به true تغییر می‌کند
-                } // پایان شرط بررسی تخفیف
-            }
-            wp_reset_postdata();
-        } 
-        if (!$has_posts) {
-         ?>
-         <style>
-            .special-offers{
-                display:none;
-            }
+    
+                    <div class="bg-[gray] rounded-lg p-1 card__actions absolute top-0 left-5 flex justify-between items-center mt-2">
+                        <!-- دکمه + -->
+                        <button
+                            class="add-to-cart bg-[gray] text-white flex justify-center items-center text-2xl   h-[35px] w-[35px]  rounded-lg"
+                            data-food-id="<?php echo esc_attr(get_the_ID()); ?>"
+                            data-food-price="<?php echo esc_attr($discount_price); ?>"
+                            data-food-title="<?php echo esc_attr($food_title); ?>"
+                            data-food-image="<?php echo esc_attr($food_image); ?>">
+                            <i class="fa fa-plus text-lg" aria-hidden="true"></i>
+                        </button>
+    
+                        <!-- اینپوت تعداد و دکمه‌های + و - -->
+                        <div class="quantity-input flex justify-center !items-center hidden items-center">
+                            <button class="decrease bg-[gray] text-white flex justify-center items-center text-2xl h-[35px] w-[35px] rounded-lg">
+                                <i class="fa fa-minus" aria-hidden="true"></i>
+                            </button>
+                            <input type="number" id="quantity_<?php echo esc_attr(get_the_ID()); ?>" name="quantity" min="1" value="1" class="h-[35px] w-[35px] countcart rounded-lg bg-[#F7F8F9] flex justify-center items-center text-center" readonly>
+                            <button class="increase bg-[gray] text-white flex justify-center items-center text-2xl h-[35px] w-[35px] rounded-lg">
+                                <i class="fa fa-plus text-lg" aria-hidden="true"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
-         </style>
-         <?php
+</div>
+<?php
+                 $has_posts = true; // اگر حلقه اجرا شد، متغیر به true تغییر می‌کند
+            } // پایان شرط بررسی تخفیف
         }
-        ?>
-        
-    </div>
-</section>
+        wp_reset_postdata();
+    } 
+    if (!$has_posts) {
+     ?>
+     <style>
+        .spdiv{
+            display:none;
+        }
+
+     </style>
+     <?php
+    }
+    ?>
+    
+</div>
+
+</div>
+
+</div>
+
+
+<!-- Initialize Swiper -->
+<script>
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    autoplay: {
+delay: 3000, // مدت زمان بین هر تغییر اسلاید (۵۰۰۰ میلی‌ثانیه = ۵ ثانیه)
+  },
+  spaceBetween: 10,
+  loop:true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+</script>
+
--- header.php
+++ header.php
@@ -6,6 +6,8 @@
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
     <?php wp_head() ?>
 </head>
